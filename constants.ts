d523e2c8424f0844417ce79c7719cb7915cee987
Kenn Henzly Ciar <ciarkenken12@gmail.com>
2025-12-07 12:13:40 +0800

    chore: serve logo and background from public assets

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
-export const LOGO_URL = 'church-logo.png';
+export const LOGO_URL = '/church-logo.png';
 export const LOGO_FALLBACK =
   'https://images.unsplash.com/photo-1548625149-fc4a29cf7092?ixlib=rb-4.0.3&auto=format&fit=crop&w=150&q=80';
